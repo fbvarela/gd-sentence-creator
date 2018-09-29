e96d3280a87f0b11d07e2290855bc007216bac20
fbvarela <fbvarela@gmail.com>
2018-09-29 11:40:29 +0200

    spring boot app

---
+++ src/main/java/com/gd/demo/AppSentencesApplication.java
@@ -0,0 +1,17 @@
+package com.gd.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSentencesApplication {
+
+    @Autowired
+    private
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppSentencesApplication.class, args);
+    }
+}
